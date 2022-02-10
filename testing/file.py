cf64399965a804f9e797e3d004069556bfa7c5fd
Dylan <dylanschutte10@gmail.com>
2022-02-10 10:00:23 +0200

    decoration of functions

---
+++ testing/file.py
@@ -0,0 +1,26 @@
+
+def dec(func):
+    def some_func():
+        print("This is before")
+        func()
+        print("This is after")
+    return some_func
+
+@dec
+def greet():
+    print("Hello Henry")
+
+greet()
+
+
+def decoration(func):
+    def function():
+        return func()**3
+    return function
+
+@decoration
+def num():
+    return 7
+
+var = num()
+print(var)
